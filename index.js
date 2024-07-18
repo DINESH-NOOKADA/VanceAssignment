ee7d81dee9f1b2b0842acab7f2ffe55168674cd7
dineshnookada <dineshnookada908@gmail.com>
2024-07-18 11:25:58 +0530

    changes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const forexDataRoutes = require('./src/routes/forexData');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 6010;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
